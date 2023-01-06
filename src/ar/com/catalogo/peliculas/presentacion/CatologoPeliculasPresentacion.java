05d098f473307a63a99cf98ef7549b3b2dc38077
Alejandro Depetris <aledepetris@outlook.com>
2023-01-06 16:20:26 -0300

    Presentacion

---
+++ src/ar/com/catalogo/peliculas/presentacion/CatologoPeliculasPresentacion.java
@@ -0,0 +1,51 @@
+package ar.com.catalogo.peliculas.presentacion;
+
+import ar.com.catalogo.peliculas.negocio.CatalogoPeliculasImpl;
+import ar.com.catalogo.peliculas.negocio.ICatalogoPeliculas;
+
+import java.util.Scanner;
+
+public class CatologoPeliculasPresentacion {
+
+    public static void main(String[] args) {
+        var opcion = -1;
+        var scanner = new Scanner(System.in);
+        ICatalogoPeliculas catalogo = new CatalogoPeliculasImpl();
+
+        while(opcion != 0){
+            System.out.println("Elige una opcion: \n"
+                    + "1. Iniciar catalogo peliculas\n"
+                    + "2. Agregar pelicula\n"
+                    + "3. Listar peliculas\n"
+                    + "4. Buscar pelicula\n"
+                    + "0. Salir");
+            opcion = Integer.parseInt(scanner.nextLine());
+
+            switch(opcion){
+                case 1:
+                    catalogo.iniciarCatalogoPeliculas();
+                    break;
+                case 2:
+                    System.out.println("Introduce el nombre de la pelicula");
+                    var nombrePelicula = scanner.nextLine();
+                    catalogo.agregarPelicula(nombrePelicula);
+                    break;
+                case 3:
+                    catalogo.listarPeliculas();
+                    break;
+                case 4:
+                    System.out.println("Introduce una pelicula a buscar");
+                    var buscar = scanner.nextLine();
+                    catalogo.buscarPelicula(buscar);
+                    break;
+                case 0:
+                    System.out.println("Hasta Pronto!");
+                    break;
+                default:
+                    System.out.println("Opcion no reconocida");
+                    break;
+            }
+        }
+    }
+
+}
